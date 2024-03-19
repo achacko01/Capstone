faa028be29bbb44ea4809de01011c6e04926739d
achacko01 <80125224+achacko01@users.noreply.github.com>
2024-03-19 00:19:15 -0400

    Update App.js

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './LoginScreen';
 import Homepage from './Homepage'; // Import your Homepage component
+import FacultyHomepage from './FacultyHomepage';
 
 const Stack = createStackNavigator();
 
@@ -12,6 +13,7 @@ export default function App() {
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
         <Stack.Screen name="Homepage" component={Homepage} options={{ title: 'Homepage' }} />
+        <Stack.Screen name="FacultyHomepage" component={FacultyHomepage} options={{ title: 'Faculty Homepage '}} />
       </Stack.Navigator>
     </NavigationContainer>
   );
