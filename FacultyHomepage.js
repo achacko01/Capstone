14a5b8693d2913b4391df9ec101e78bb055d510a
achacko01 <80125224+achacko01@users.noreply.github.com>
2024-04-03 22:54:40 -0400

    Update FacultyHomepage.js with Add/Remove Machine

--- FacultyHomepage.js
+++ FacultyHomepage.js
@@ -5,10 +5,10 @@ const FacultyHomepage = ({ navigation }) => {
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <Text style={styles.title}>Faculty Portal</Text>
-      <TouchableOpacity style={styles.button} onPress={() => console.log('Add Machine')}>
+      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('AddMachine')}>
         <Text style={styles.buttonText}>Add Machine</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={() => console.log('Remove Machine')}>
+      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('RemoveMachine')}>
         <Text style={styles.buttonText}>Remove Machine</Text>
       </TouchableOpacity>
       <TouchableOpacity style={styles.button} onPress={() => console.log('Machine Use History')}>
@@ -22,35 +22,35 @@ const FacultyHomepage = ({ navigation }) => {
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flexGrow: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#D9DADE',
-        borderTopWidth: 5,
-        borderTopColor: '#00703C',
-        borderBottomWidth: 5,
-        borderBottomColor: '#00703C',
-        paddingVertical: 20,
-    },
-    title: {
-        fontSize: 24,
-        marginBottom: 20,
-        color: '#000', // Adjust color as needed
-    },
-    button: {
-        width: '80%',
-        padding: 10,
-        marginVertical: 10,
-        backgroundColor: '#00703C',
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 5,
-    },
-    buttonText: {
+  container: {
+    flexGrow: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#D9DADE',
+    borderTopWidth: 5,
+    borderTopColor: '#00703C',
+    borderBottomWidth: 5,
+    borderBottomColor: '#00703C',
+    paddingVertical: 20,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+    color: '#000', // Adjust color as needed
+  },
+  button: {
+    width: '80%',
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#00703C',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  buttonText: {
     color: '#FFFFFF',
     fontSize: 16,
-    },
+  },
 });
 
 export default FacultyHomepage;
