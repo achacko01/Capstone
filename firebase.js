dc59fec29a5228878df5deec308bd271d2f52a93
Toby Nguyen <152452414+tnguyen7301@users.noreply.github.com>
2024-04-24 17:22:41 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,21 +1,25 @@
-import { initializeApp } from '@react-native-firebase/app';
-import { getAuth } from '@react-native-firebase/auth';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/compat/analytics';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
 
-// Firebase configuration object
 const firebaseConfig = {
-  apiKey: 'AIzaSyCQxp3OV_OyjzdPgkp4uWmtSvZwcs61VcQ',
-  authDomain: 'capstonegymapp.firebaseapp.com',
-  projectId: 'capstonegymapp',
-  storageBucket: 'capstonegymapp.appspot.com',
-  messagingSenderId: '42204156247',
-  appId: '1:42204156247:ios:00dceff74e8052c8afae12',
-  databaseURL: 'https://capstonegymapp-default-rtdb.firebaseio.com'
+  apiKey: "AIzaSyBh5aSvmD-P9zbdyEruAZjr3Xk9Il_ySPk",
+  authDomain: "capstone-authentication-57df3.firebaseapp.com",
+  databaseURL: "https://capstone-authentication-57df3-default-rtdb.firebaseio.com",
+  projectId: "capstone-authentication-57df3",
+  storageBucket: "capstone-authentication-57df3.appspot.com",
+  messagingSenderId: "1324354770",
+  appId: "1:1324354770:web:8470a1f59c10cd0b49b10b",
+  measurementId: "G-9PLK93NBCD"
 };
 
-// Initialize Firebase app
-const firebaseApp = initializeApp(firebaseConfig);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
-// Get Firebase Auth instance
-export const firebaseAuth = getAuth(firebaseApp);
-
-export default firebaseApp;
+// Export the Firebase instance and other services
+export { firebase, app, analytics };
